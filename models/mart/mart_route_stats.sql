ca30b997bed8f8825c97ed7fd0e5f8e696e86ed4
Nazd <debevec.zan@gmail.com>
2025-07-15 11:27:12 +0200

    fix 2

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -11,7 +11,7 @@ WITH calculations AS
 		sum(diverted) AS count_diverted
 FROM {{ref('prep_flights')}}
 GROUP BY origin, dest)
-SELECT origin, a.city, a.country, a.name, dest, a2.city, a2.country, a2.name, count_flights, count_unique_planes,
+SELECT origin, a.city as origin_city, a.country as origin_country, a.name as origin_name, dest, a2.city as dest_city, a2.country as dest_country, a2.name as dest_name, count_flights, count_unique_planes,
 count_unique_airline, avg_actual_elapsed_time, avg_arrival_delay, max_arr_delay, min_arr_delay, count_cancelled, count_diverted
 FROM calculations
 LEFT JOIN {{ref('prep_airports')}} a ON origin = a.faa
