348efa79a85d6b12f3dc425f46b5f6e620ed1b15
Nazd <debevec.zan@gmail.com>
2025-07-16 10:14:34 +0200

    5.5.

---
+++ models/mart/mart_faa_weather_cancelled_diverted_flights.sql
@@ -0,0 +1,19 @@
+WITH airport_flights AS (
+			SELECT flight_date, origin, name, city, country,
+			sum(cancelled) AS sum_cancelled,
+			sum(diverted) AS sum_diverted
+FROM {{ref('prep_airports')}}
+JOIN {{ref('prep_flights')}}
+ON faa = origin
+GROUP BY flight_date, origin, name, city, country)
+SELECT flight_date, origin, name, city, country,
+			sum_cancelled,
+			sum_diverted,
+			avg(avg_temp_c) AS avg_temp_c,
+			avg(precipitation_mm) AS avg_precipitation_mm,
+			avg(max_snow_mm) AS max_snow_mm,
+			avg(avg_wind_speed_kmh) AS avg_wind_speed_kmh
+FROM airport_flights
+right JOIN {{ref('prep_weather_daily')}}
+ON airport_code = ORIGIN
+GROUP BY flight_date, origin, name, city, country, sum_cancelled, sum_diverted;
